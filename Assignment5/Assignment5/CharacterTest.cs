0c40f290999c59ec85880398c54ec38aa748c9c1
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2022-09-01 22:10:44 -0700

    Added Unity Tests

---
+++ Assignment5/Assignment5/CharacterTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Assignment5
+{
+    [TestFixture]
+    public class CharacterTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+
+        }
+
+        [Test]
+        public void TakeDamageCorrectHealth()
+        {
+
+        }
+
+        [Test]
+        public void TakeDamageIsDead()
+        {
+
+        }
+    }
+}
---
+++ Assignment5/Assignment5/InventoryTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Assignment5
+{
+    [TestFixture]
+    public class InventoryTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+
+        }
+
+        [Test]
+        public void TakeDamage()
+        {
+
+        }
+    }
+}
